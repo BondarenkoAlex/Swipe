adddd8dc23e26a04758142961ff95482d892584a
Bondik <bondarenkoalax@mail.ru>
2017-08-30 22:37:12 +0300

    commit

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ module.exports = {
   plugins: {
     /*'postcss-import': {},
     'postcss-cssnext': {},*/
-    'autoprefixer': {browsers:["last 2 version", "Firefox 15"]},
+    'autoprefixer': {browsers:["> 2%","last 5 version"]},
     /*'cssnano': {}*/
   }
 };
